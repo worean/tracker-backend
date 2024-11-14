40efcef5270949b372cf8a020b2fc6181c53c0bd
juyeong.park <qkqwnjdud@gmail.com>
2024-11-14 19:58:32 +0900

    chat 라우팅에 불필요한 데이터 제거

--- src/routes/chat/chat.ts
+++ src/routes/chat/chat.ts
@@ -7,14 +7,25 @@ const router = express.Router();
 // 특정 채팅방의 메시지를 조회한다.
 router.get('/:chatId/messages', async (req: Request, res: Response) => {
     var chatId = parseInt(req.params.chatId);
+
+    // 현재 로그인된 사용자를 가져온다.
     var user = await GetLoginedUser(req);
     if (user == null) {
         res.status(401).json({ error: 'Unauthorized' });
         return;
     }
+
+    // 로그인된 유저가 속해있는 채팅방에서의 메시지를 가져온다. 
     var messages = await prisma.chat_Message.findMany({
         where: {
-            chatId
+            chatId,
+            chat:{
+                users:{
+                    some:{
+                        id: user.id
+                    }
+                }
+            }
         }
     });
     if (messages == null || messages.length == 0) {
@@ -36,11 +47,18 @@ router.get('/:chatId/message/:messageId', async (req: Request, res: Response) =>
         return;
     }
 
-    // 메시지를 가져온다.
+    // 로그인된 유저가 속해있는 채팅방에서의 메시지를 가져온다.
     var message = await prisma.chat_Message.findUnique({
         where: {
             id: messageId,
-            chatId : chatId
+            chat : {
+                id: chatId,
+                users:{
+                    some:{
+                        id: user.id
+                    }
+                }
+            }
         }
     });
 
@@ -51,6 +69,5 @@ router.get('/:chatId/message/:messageId', async (req: Request, res: Response) =>
 
     res.status(200).json(message);
 });
-
 
 export default router;
