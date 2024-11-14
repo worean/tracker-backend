68058cf94489c0fb1e58e9117ef95d9384aaa6b2
juyeong.park <qkqwnjdud@gmail.com>
2024-11-14 18:22:42 +0900

    chat 관련 routing 추가

---
+++ src/routes/chat/chat.ts
@@ -0,0 +1,56 @@
+import express, { Request, Response } from 'express';
+import prisma from '../../client';
+import { GetLoginedUser } from '../../utils/auth';
+
+const router = express.Router();
+
+// 특정 채팅방의 메시지를 조회한다.
+router.get('/:chatId/messages', async (req: Request, res: Response) => {
+    var chatId = parseInt(req.params.chatId);
+    var user = await GetLoginedUser(req);
+    if (user == null) {
+        res.status(401).json({ error: 'Unauthorized' });
+        return;
+    }
+    var messages = await prisma.chat_Message.findMany({
+        where: {
+            chatId
+        }
+    });
+    if (messages == null || messages.length == 0) {
+        res.status(404).json({ error: 'No messages found' });
+    }
+    res.status(200).json(messages);
+});
+
+// 특정 채팅방의 특정 메시지를 조회한다.
+router.get('/:chatId/message/:messageId', async (req: Request, res: Response) => {
+    // 채팅방 아이디와 메시지 아이디를 가져온다.
+    var messageId = parseInt(req.params.messageId);
+    var chatId = parseInt(req.params.chatId);
+
+    // 현재 로그인된 사용자를 가져온다.
+    var user = await GetLoginedUser(req);
+    if (user == null) {
+        res.status(401).json({ error: 'Unauthorized' });
+        return;
+    }
+
+    // 메시지를 가져온다.
+    var message = await prisma.chat_Message.findUnique({
+        where: {
+            id: messageId,
+            chatId : chatId
+        }
+    });
+
+    // 메시지가 없다면 404를 반환한다.
+    if (message == null) {
+        res.status(404).json({ error: 'Message not found' });
+    }
+
+    res.status(200).json(message);
+});
+
+
+export default router;
---
+++ src/routes/chat/chat_message.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express';
+import prisma from '../../client';
+import { GetLoginedUser } from '../../utils/auth';
+
+const router = express.Router();
+
+
+
+
+export default router;
